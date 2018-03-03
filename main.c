d96a9da1710d692ec0ab727951af74577e55a226
George Ridgway <georger361@gmail.com>
2018-03-03 17:14:01 +0000

    finished ls implementation

--- main.c
+++ main.c
@@ -2,14 +2,33 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 
 /* TO implement in the 'basic version'
- * ls 
  * pwd
  * echo
  * sleep
  *quit program on ^D
  */
+
+/* Function that handles the functionality of the 'ls' command */
+void ls(){
+  /* Going to handle the opening of the files */
+  DIR *dirStream;
+  struct dirent *dir;
+  dirStream = opendir(".");
+  /* If we actually have someathing in the current dir to open */
+  if (dirStream) {
+    while ((dir = readdir(dirStream)) != NULL) {
+    	/* We dont want to show hidden files */ 
+	if (dir->d_name[0] != '.'){
+		 printf(" %s ", dir->d_name);
+    	}
+    }
+    closedir(dirStream);
+  }	
+}
+
 
 int main(){
 	bool run = true;
@@ -35,12 +54,21 @@ int main(){
 		for (int i = 0; i <= len; i++){
 			compareMe[i] = userInput[i];
 		}
+		
 		/* comparing what the user typed vs what we are looking for */
-		if (strcmp(compareMe, "ls") == 0){
-			printf("You just entered: %s",userInput);
+		if (strcmp(compareMe, "ls") == 0){ //if the user wants to list the files in the dir
+			ls();
+			printf("\n");	
 		}
+		else if (len == 2){ //if the user inputted 'pwd'
 
+		}
+		else if (len > 2){ //if the user did not
+			printf("This is what you entered: %s\n",compareMe);
+		}
 		free(compareMe);	
-		printf("\n");
 	}
 }
+
+
+
