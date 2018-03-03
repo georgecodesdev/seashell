699a4c6efd23c592f78eea75d08692b794951168
George Ridgway <georger361@gmail.com>
2018-03-03 14:53:18 +0000

    still working on getting the userinput to actally work -- todo actually determine what the userInput is

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+/* TO implement in the 'basic version'
+ * ls 
+ * pwd
+ * echo
+ * sleep
+ *quit program on ^D
+ */
+
+int main(int argc, char *argv){
+	bool run = true;
+	char *userInput;
+	int bufSize = 100;
+		
+	char *checkMe = "ls";
+	userInput = (char *)malloc(bufSize * sizeof(char));
 	
+	while (run){
+		printf("# ");
+		
+		if (fgets(userInput,bufSize,stdin) == NULL){
+			printf("\n");
+			break;
+		}
 
+		int len = strlen(userInput) - 2; //for some reason the strlen doesnt actually get the correct num chars -- idk why
+		printf("\n\nWhat does this do %c\n\n",userInput[len]);
+		
+		if (strcmp(userInput, checkMe) == 0){
+			printf("You just entered: %s",userInput);
+			printf("\n");
+		}
+		
+	}
 }
